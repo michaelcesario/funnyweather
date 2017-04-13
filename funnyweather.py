2b4fbd6fdaf9a06a927b5e6b79d5aec7a4a07aaa
michaelcesario <michael_c_4@live.ca>
2017-04-13 12:56:49 -0400

    added flask

--- funnyweather.py
+++ funnyweather.py
@@ -1,5 +1,5 @@
 from pyrebase import pyrebase
-
+from flask import Flask, jsonify, request
 config = {
   "apiKey": "AIzaSyCOsBU3ROQlKhuikpRbagIJxl1P7OATfmY",
   "authDomain": "funnyweather-1cf97.firebaseapp.com",
@@ -14,3 +14,20 @@ db = firebase.database()
 db.child("cities").push("Toronto")
 
 print("Hello, World!")
+
+app = Flask(__name__) #global object for file
+
+@app.route("/hello") #endpoint that clients reach via route
+def hello(): #function that will run when client reaches endpoint
+	return "Hello World!";
+
+@app.route("/add_user")#, methods = ['POST'])
+def add_user():
+	#dict = request.get_json() #save incoming json data from client into dictionary
+	#username = dict['username']
+	#email = dict['email']
+	db.child("cities").push("Toronto")	
+	
+
+if __name__ == "__main__": 
+	app.run()
