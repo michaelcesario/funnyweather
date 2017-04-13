29614c960c274dc4409d51b1d29d725ab3349641
michaelcesario <michael_c_4@live.ca>
2017-04-12 21:11:06 -0400

    test firebase

--- funnyweather.py
+++ funnyweather.py
@@ -1,1 +1,16 @@
+import pyrebase
+
+config = {
+  "apiKey": "AIzaSyCOsBU3ROQlKhuikpRbagIJxl1P7OATfmY",
+  "authDomain": "funnyweather-1cf97.firebaseapp.com",
+  "databaseURL": "https://funnyweather-1cf97.firebaseio.com/",
+  "storageBucket": "funnyweather-1cf97.appspot.com"
+}
+
+firebase = pyrebase.initialize_app(config)
+
+db = firebase.database()
+
+db.child("cities").push("Toronto")
+
 print("Hello, World!")
