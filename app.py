81d9c299ec68aef7f18b74fc97635726f00cddcb
michaelcesario <michael_c_4@live.ca>
2017-04-18 01:15:19 -0400

    push

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from pyrebase import pyrebase
 from flask import Flask, jsonify, request
+from apns import APNs, Frame, Payload
 
 app = Flask(__name__) #global object for file
 
@@ -15,13 +16,26 @@ firebase = pyrebase.initialize_app(config)
 
 db = firebase.database()
 
-db.child("cities").push("Test")
+apns = APNs(use_sandbox=True, cert_file='aps_dev_cert.pem', key_file='aps_dev_key.pem')
+
+# Send an iOS 10 compatible notification
+token_hex = 'd69bd4faa2beca2ff103fb172643d7dfd66304187afcb63cc0ab417856447d33'
+payload = Payload(alert="Hello World!", sound="default", badge=1, mutable_content=True)
+apns.gateway_server.send_notification(token_hex, payload)
+
+# Send multiple notifications in a single transmission
+frame = Frame()
+identifier = 1
+expiry = time.time()+3600
+priority = 10
+frame.add_item('b5bb9d8014a0f9b1d61e21e796d78dccdf1352f23cd32812f4850b87', payload, identifier, expiry, priority)
 
 
 
 
 @app.route("/hello") #endpoint that clients reach via route
 def hello(): #function that will run when client reaches endpoint
+    apns.gateway_server.send_notification_multiple(frame)
 	return "Hello World!";
 
 @app.route("/add_city", methods = ['POST'])
